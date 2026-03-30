5063975ea6c9c020174d787f1b2301ec4fcc17ae
bishal10rm <talukdarbishal68@gmail.com>
2026-03-31 02:49:45 +0600

    Added Banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import cartImg from "../assets/products/shopping-cart.png";
+import bannerImg from "../assets/banner.png";
 
 function App() {
  
@@ -9,7 +10,7 @@ function App() {
     <>
      
 
-      <div className="navbar bg-base-100 shadow-sm ">
+      <div className="navbar bg-base-100  container mx-auto">
   <div className="navbar-start">
     <a className="text-3xl font-bold text-[#4F39F6]">DigiTools</a>
   </div>
@@ -27,6 +28,27 @@ function App() {
     <img src={cartImg} alt="cart"></img>
     <a >Login</a>
     <a className="btn bg-[#4F39F6] text-white  rounded-4xl ">Get Started</a>
+  </div>
+</div>
+
+  {/* banner part start  */}
+
+  <div className=" container mx-auto ">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src={bannerImg} alt="banner"></img>
+    <div>
+      <div className="badge badge-soft badge accent text-[#4F39F6]">New: AI-Powered Tools Available</div>
+      <h1 className="text-7xl font-extrabold">Supercharge Your Digital Workflow</h1>
+      <p className="py-6 text-[#627382">
+       Access premium AI tools, design assets, templates, and productivity<br></br>
+       software—all in one place. Start creating faster today.<br></br>
+       Explore Products
+      </p>
+      <div className="gap-3">
+      <button className="btn btn-primary rounded-4xl text-white ">Explore Products</button>
+      <button className="btn btn-outline border-[#4F39F6]  rounded-4xl">Watch Demo</button>
+      </div>
+    </div>
   </div>
 </div>
     </>
